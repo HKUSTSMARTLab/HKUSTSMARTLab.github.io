509044b12762a49d9e00176324285f3d91ec00d2
HKUSTSMARTLab <112254853+HKUSTSMARTLab@users.noreply.github.com>
2024-05-22 13:07:14 +0800

    Feat: Amend homepage, carousel

---
+++ _layouts/script.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images img');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// 初始化显示第一张图片
+showSlide(currentSlide);
