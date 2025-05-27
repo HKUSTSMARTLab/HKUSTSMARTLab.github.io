00e403071fcd5b87eb031a453c2f22a7da58bec1
HKUSTSMARTLab <51412849+MrPeterJin@users.noreply.github.com>
2025-05-28 03:46:35 +0800

    Chore: PathBench

--- static/js/citations/citations-data.js
+++ static/js/citations/citations-data.js
@@ -4,14 +4,14 @@
  */
 const citationsData = [
     {
-        id: 'he2024towards',
-        title: 'He and Nie et al. (2024) - GSCo: Towards Generalizable AI in Medicine',
-        bibtex: `@article{he2024towards,
-    title = {GSCo: Towards Generalizable AI in Medicine via Generalist-Specialist Collaboration},
-    author = {He, Sunan and Nie, Yuxiang and Wang, Hongmei and Yang, Shu and Wang, Yihui and Cai, Zhiyuan and Chen, Zhixuan and Xu, Yingxue and Luo, Luyang and Xiang, Huiling and Lin, Xi and Wu, Mingxiang and Peng, Yifan and Shih, George and Xu, Ziyang and Wu, Xian and Wang, Qiong and Chan, Ronald Cheong Kin and Vardhanabhuti, Varut and Chu, Winnie Chiu Wing and Zheng, Yefeng and Rajpurkar, Pranav and Zhang, Kang and Chen, Hao},
-    journal = {arXiv preprint arXiv:2404.15127},
-    year = {2024}
-  }`,
+        id: 'ma2025pathbench',
+        title: 'Ma, Xu et al. (2025) - Pathology Foundation Model Benchmark',
+        bibtex: `@article{ma2025pathbench,
+      title={PathBench: A comprehensive comparison benchmark for pathology foundation models towards precision oncology},
+      author={Ma, Jiabo and Xu, Yingxue and Zhou, Fengtao and Wang, Yihui and Jin, Cheng and Guo, Zhengrui and Wu, Jianfeng and Tang, On Ki and Zhou, Huajun and Wang, Xi and Luo, Luyang and Zhang, Zhengyu and Cai, Du and Gao, Zizhao and Wang, Wei and Liu, Yueping and He, Jiankun and Cui, Jing and Li, Zhenhui and Zhang, Jing and Gao, Feng and Zhang, Xiuming and Liang, Li and Chan, Ronald Cheong Kin and Wang, Zhe and Chen, Hao},
+      journal={arXiv preprint arXiv:2505.20202},
+      year={2025}
+    }`,
     },
     {
         id: 'jiang2025generalizable',
@@ -21,6 +21,16 @@ const citationsData = [
     author = {Jiang, Hao and Jin, Cheng and Lin, Huangjing and Zhou, Yanning and Wang, Xi and Ma, Jiabo and Ding, Li and Hou, Jun and Liu, Runsheng and Chai, Zhizhong and Luo, Luyang and Shi, Huijuan and Qian, Yinling and Wang, Qiong and Li, Changzhong and Han, Anjia and Chan, Ronald Cheong Kin and Chen, Hao},
     journal = {arXiv preprint arXiv:2502.09662},
     year = {2025}
+  }`,
+    },
+    {
+        id: 'he2024towards',
+        title: 'He and Nie et al. (2024) - GSCo: Towards Generalizable AI in Medicine',
+        bibtex: `@article{he2024towards,
+    title = {GSCo: Towards Generalizable AI in Medicine via Generalist-Specialist Collaboration},
+    author = {He, Sunan and Nie, Yuxiang and Wang, Hongmei and Yang, Shu and Wang, Yihui and Cai, Zhiyuan and Chen, Zhixuan and Xu, Yingxue and Luo, Luyang and Xiang, Huiling and Lin, Xi and Wu, Mingxiang and Peng, Yifan and Shih, George and Xu, Ziyang and Wu, Xian and Wang, Qiong and Chan, Ronald Cheong Kin and Vardhanabhuti, Varut and Chu, Winnie Chiu Wing and Zheng, Yefeng and Rajpurkar, Pranav and Zhang, Kang and Chen, Hao},
+    journal = {arXiv preprint arXiv:2404.15127},
+    year = {2024}
   }`,
     },
     {
